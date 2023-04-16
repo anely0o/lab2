9ae18c3431c0949c62c614850ca98993a921020f
anely0o <ramyun.oppar@gmail.com>
2023-04-16 12:33:19 +0600

    emae2

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,10 +1,19 @@
-public class MyArrayList {
+import java.util. *;
+public abstract class MyArrayList<E> implements List<E> {
     private Object[] elements;
 
     public MyArrayList(int initialCapasity) {
         this.elements = new Object[initialCapasity];
     }
     private int size;
-    
+    private void ensureCapacity(int minCapacity) {
+        int oldCapacity = elements.length;
+        if (minCapacity > oldCapacity) {
+            int newCapacity = oldCapacity * 2;
+            if (newCapacity < minCapacity)
+                newCapacity = minCapacity;
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
 
 }
