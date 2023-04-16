d704130f6c7aac65e4defe9474c0c9c9ca576889
anely0o <ramyun.oppar@gmail.com>
2023-04-16 15:21:18 +0600

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -56,6 +56,8 @@ public class MyLinkedList<E> implements List<E> {
         }else{
             current.next.prev = current.prev;
         }
+        size--;
+        return current.element;
 
         }
 
