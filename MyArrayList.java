bfac474b83d5d588d2d42e089acd331f03fe1d7b
anely0o <ramyun.oppar@gmail.com>
2023-04-16 12:52:05 +0600

    bruh

--- MyArrayList.java
+++ MyArrayList.java
@@ -38,5 +38,110 @@ public abstract class MyArrayList<E> implements List<E> {
     public int size() {
         return size;
     }
+    public static void main(String[] args) {
+        MyArrayList<Integer> myList = new MyArrayList<>() {
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Integer> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Integer> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends Integer> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Integer set(int index, Integer element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, Integer element) {
+
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Integer> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<Integer> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<Integer> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        System.out.println(myList.get(1)); // Output: 2
+        myList.remove(1);
+        System.out.println(myList.get(1)); // Output: 3
+        System.out.println(myList.size()); // Output: 2
+    }
 
 }
